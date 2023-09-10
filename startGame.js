24bcdf073f5e0651cbff5a833e8741a134899b19
Aviator2276 <91645065+Aviator2276@users.noreply.github.com>
2023-09-10 19:10:56 -0400

    Finished working on startGame and teamConfig stages of the Game

---
+++ startGame.js
@@ -0,0 +1,70 @@
+const startGame = document.querySelector("#startGame");
+
+function createRandomDot() {
+    const dot = document.createElement('div');
+    dot.classList.add('dot');
+    
+    const randomX = Math.random() * window.innerWidth;
+    const randomY = Math.random() * window.innerHeight;
+    
+    dot.style.left = `${randomX}px`;
+    dot.style.top = `${randomY}px`;
+    
+    document.getElementById('hyperSpace').appendChild(dot);
+    
+    setTimeout(() => {
+        transformDotToLine(dot);
+    }, 1200);
+}
+function transformDotToLine(dot) {
+    dot.classList.remove('dot');
+    dot.classList.add('line');
+
+    const squareTrans = document.getElementById('transition');
+
+    const dotX = parseFloat(dot.style.left) + 5; // 5 is half of dot's width
+    const dotY = parseFloat(dot.style.top) + 5; // 5 is half of dot's height
+    
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+    
+    const angle = Math.atan2(centerY - dotY, centerX - dotX) + Math.PI;
+    const distance = Math.sqrt(Math.pow(centerX - dotX, 2) + Math.pow(centerY - dotY, 2)) + 1000;
+    
+    const line = document.createElement('div');
+    line.classList.add('line');
+    line.style.transformOrigin = '0% 50%';
+    
+    line.style.position = 'absolute';
+    line.style.left = `${dotX}px`;
+    line.style.top = `${dotY}px`;
+    line.style.transform = `rotate(${angle}rad)`;
+    line.style.width = '0';
+    line.style.height = '10px';
+    line.style.zIndex = '2';
+    line.style.background = 'white';
+    line.style.borderRadius = '5px';
+    
+    document.getElementById('hyperSpace').appendChild(line);
+    
+    line.style.animation = 'extend 2s forwards';
+    squareTrans.classList.add('trans')
+    setTimeout(() => {
+        line.remove();
+    }, 10000);
+}
+function generateRandomDots(numDots) {
+    for (let i = 0; i < numDots; i++) {
+        createRandomDot();
+    }
+}
+
+
+window.addEventListener('load', function () {
+    const element = document.getElementById('wavyTextStart');
+    setTimeout(() => element.classList.add('infinite'), 2300);
+});
+startGame.onclick = () => {
+    generateRandomDots(500);
+    setTimeout(() => window.open('teamConfig.html','_self',false), 2200);
+};
---
+++ teamConfig.js
@@ -0,0 +1,94 @@
+const promptMesg = document.getElementById('Mesg');
+const oneTeam = document.getElementById('oneTeam');
+const twoTeam = document.getElementById('twoTeam');
+const threeTeam = document.getElementById('threeTeam');
+const fourTeam = document.getElementById('fourTeam');
+const transition = document.getElementById('transition');
+
+function addClassToElements(className, ...elements) {
+    elements.forEach(element => element.classList.add(className));
+}
+
+function createRandomDot() {
+    const dot = document.createElement('div');
+    dot.classList.add('dot');
+    
+    const randomX = Math.random() * window.innerWidth;
+    const randomY = Math.random() * window.innerHeight;
+    
+    dot.style.left = `${randomX}px`;
+    dot.style.top = `${randomY}px`;
+    
+    document.getElementById('hyperSpace').appendChild(dot);
+    
+    setTimeout(() => {
+        transformDotToLine(dot);
+    }, 1200);
+}
+function transformDotToLine(dot) {
+    dot.classList.remove('dot');
+    dot.classList.add('line');
+
+    const squareTrans = document.getElementById('transition');
+
+    const dotX = parseFloat(dot.style.left) + 5; // 5 is half of dot's width
+    const dotY = parseFloat(dot.style.top) + 5; // 5 is half of dot's height
+    
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+    
+    const angle = Math.atan2(centerY - dotY, centerX - dotX) + Math.PI;
+    const distance = Math.sqrt(Math.pow(centerX - dotX, 2) + Math.pow(centerY - dotY, 2)) + 1000;
+    
+    const line = document.createElement('div');
+    line.classList.add('line');
+    line.style.transformOrigin = '0% 50%';
+    
+    line.style.position = 'absolute';
+    line.style.left = `${dotX}px`;
+    line.style.top = `${dotY}px`;
+    line.style.transform = `rotate(${angle}rad)`;
+    line.style.width = '0';
+    line.style.height = '10px';
+    line.style.zIndex = '10';
+    line.style.background = 'white';
+    line.style.borderRadius = '5px';
+    
+    document.getElementById('hyperSpace').appendChild(line);
+    
+    line.style.animation = 'extend 2s forwards';
+    transition.style.animation = 'transitionOut 1.6s forwards';
+    setTimeout(() => {
+        line.remove();
+    }, 10000);
+}
+function generateRandomDots(numDots) {
+    for (let i = 0; i < numDots; i++) {
+        createRandomDot();
+    }
+}
+
+oneTeam.onclick = () => {
+    addClassToElements('hidePrompt', twoTeam, threeTeam, fourTeam, promptMesg);
+    oneTeam.classList.add('oneSel');
+    setTimeout(() => generateRandomDots(500), 200);
+    setTimeout(() => window.open('preliminary.html','_self',false), 2200);
+};
+twoTeam.onclick = () => {
+    addClassToElements('hidePrompt', oneTeam, threeTeam, fourTeam, promptMesg);
+    twoTeam.classList.add('twoSel');
+    setTimeout(() => generateRandomDots(500), 200);
+    setTimeout(() => window.open('preliminary.html','_self',false), 2200);
+};
+threeTeam.onclick = () => {
+    addClassToElements('hidePrompt', oneTeam, twoTeam, fourTeam, promptMesg);
+    threeTeam.classList.add('threeSel');
+    setTimeout(() => generateRandomDots(500), 200);
+    setTimeout(() => window.open('preliminary.html','_self',false), 2200);
+};
+fourTeam.onclick = () => {
+    addClassToElements('hidePrompt', oneTeam, twoTeam, threeTeam, promptMesg);
+    fourTeam.classList.add('fourSel');
+    setTimeout(() => generateRandomDots(500), 200);
+    setTimeout(() => window.open('preliminary.html','_self',false), 2200);
+};
