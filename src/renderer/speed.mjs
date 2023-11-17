7839edcb881e044a46faeebab0f4f5d9c0ab1483
Emmett <ectrippe08@jeffcityschools.org>
2023-11-17 12:44:05 -0500

    updated speed.mjs

--- src/renderer/speed.mjs
+++ src/renderer/speed.mjs
@@ -29,9 +29,16 @@ function getSpeedElement(elementClass) {
     return speedStage.getElementsByClassName(elementClass)[0];
 }
 
-export function onLoad() {
-    transition.classList.add('transLoadPage');
+export function onLoad(firstTime) {
+    if (firstTime) {
+        transition.classList.add('transLoadPage');
+    } else {
+        correct.style.animation = 'correctRevealSlideOut .6s forwards';
+        incorrect.style.animation = 'correctRevealSlideOut .6s forwards';
+        showAnswer.style.animation = 'showAnswerSlideOut .6s forwards'; 
+    }
     let questionAndAnswers = gameLogic.currentQuestion();
+    console.log(question);
     question.innerHTML = questionAndAnswers[0];
     ansA.innerHTML = questionAndAnswers[1];
     ansB.innerHTML = questionAndAnswers[2];
@@ -39,42 +46,37 @@ export function onLoad() {
     ansD.innerHTML = questionAndAnswers[4];
     correctAnswer.innerHTML = gameLogic.getAnswer();
 }
+
 function nextPage() {
-    renderer.generateRandomDots(800)
-    setTimeout(() => gameLogic.updateGame("teamSelectStage"), 2200);
+    renderer.generateRandomDots(400)
+    setTimeout(() => gameLogic.updateGame("speedStage"), 2200);
 }
 
 function resetPage() {
-    addClassToElements('hidden', showAnswer, correct, incorrect);
+    removeClassFromElements('hidePrompt', ansA, ansB, ansC, ansD, question);
+    removeClassFromElements('transLoadPage', transition);
+    removeClassFromElements('selected', ansA, ansB, ansC, ansD);
     correct.style.animation = ''; 
     incorrect.style.animation = ''; 
-    showAnswer.style.animation = '';
+    showAnswer.style.animation = ''; 
+    addClassToElements('hidden', showAnswer, correct, incorrect);
 }
 
 function guessAnswer(guess) {
-    const transition = document.getElementById(gameLogic.getGameStage()).getElementsByClassName('transition')[0];
     //showAnswer.classList.add('confirmShow');
     showAnswer.classList.remove('hidden');
     if (guess.innerHTML === correctAnswer.innerHTML) {
         correct.classList.remove('hidden');
+        
     } else {
         incorrect.classList.remove('hidden');
     }
 
     setTimeout(() => {
-        removeClassFromElements('hidePrompt', ansA, ansB, ansC, ansD, question);
-        removeClassFromElements('transLoadPage', transition);
-        removeClassFromElements('selected', ansA, ansB, ansC, ansD);
-        correct.style.animation = ''; 
-        incorrect.style.animation = ''; 
-        showAnswer.style.animation = ''; 
-        correct.style.animation = 'correctRevealSlideOut .6s forwards';
-        incorrect.style.animation = 'correctRevealSlideOut .6s forwards';
-        showAnswer.style.animation = 'showAnswerSlideOut .6s forwards'; 
         setTimeout(() => {
             resetPage();
             gameLogic.nextQuestion();
-            onload();
+            onLoad(false);
         }, 600);
         //correct.style.animation = 'correctRevealSlideOut 1s ';
         //incorrect.style.animation = 'correctRevealSlideOut 1s ';
@@ -86,24 +88,20 @@ function guessAnswer(guess) {
 ansA.onclick = () => {
     addClassToElements('hidePrompt', ansB, ansC, ansD, question);
     ansA.classList.add('selected');
-    confirmMesg.innerHTML = "Confirm 1 Team Selection";
     setTimeout(() => guessAnswer(ansA), 500);
 };
 ansB.onclick = () => {
     addClassToElements('hidePrompt', ansA, ansC, ansD, question);
     ansB.classList.add('selected');
-    confirmMesg.innerHTML = "Confirm 2 Team Selection";
     setTimeout(() => guessAnswer(ansB), 500);
 };
 ansC.onclick = () => {
     addClassToElements('hidePrompt', ansA, ansB, ansD, question);
     ansC.classList.add('selected');
-    confirmMesg.innerHTML = "Confirm 3 Team Selection";
     setTimeout(() => guessAnswer(ansC), 500);
 };
 ansD.onclick = () => {
     addClassToElements('hidePrompt', ansA, ansB, ansC, question);
     ansD.classList.add('selected');
-    confirmMesg.innerHTML = "Confirm 4 Team Selection";
     setTimeout(() => guessAnswer(ansD), 500);
 };
